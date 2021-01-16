2632ecfb183e7511c3f97574f150d09053163bd6
Rodion <asvrodya@gmail.com>
2021-01-17 01:02:21 +0200

    Add .gitignore | Implement webpack cfg for pug and scss

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+}
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        index: 'index.html',
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+        writeToDisk: true,
+        open: 'chrome',
+        compress: true,
+        port: 8888
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {pretty: true},
+                exclude: '/node_modules/',
+            },
+            {
+                test:/\.(s*)css$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+    new CleanWebpackPlugin(),
+    new miniCss({
+        filename: 'css/style.css',
+    }),
+    ...PAGES.map(page => new HtmlWebpackPlugin ({
+        template: `${PAGES_DIR}/${page}`,
+        filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
+   ]
+};
